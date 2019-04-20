7f7dddc23e3410d7866e8eca98cc386a4a401170
xdream <xdream@gmail.com>
2019-04-20 22:59:28 +0800

    学习爬虫中:requests完善zhibo8抓取

--- GUItest.py
+++ GUItest.py
@@ -8,27 +8,26 @@ import pandas as pd
 import colorlover as cl
 
 colors = cl.scales['5']['seq']['Blues']
-data = {'Year' : [2010, 2011, 2012, 2013, 2014],
-        'Color' : colors}
+data = {'Year': [2010, 2011, 2012, 2013, 2014],
+        'Color': colors}
 df = pd.DataFrame(data)
 
-
 trace0 = go.Table(
-  header = dict(
-    values = ["Color", "<b>YEAR</b>"],
-    line = dict(color = 'white'),
-    fill = dict(color = 'white'),
-    align = ['center'],
-    font = dict(color = 'black', size = 12)
-  ),
-  cells = dict(
-    values = [df.Color, df.Year],
-    line = dict(color = [df.Color]),
-    fill = dict(color = [df.Color]),
-    align = 'center',
-    font = dict(color = 'black', size = 11)
+    header=dict(
+        values=["Color", "<b>YEAR</b>"],
+        line=dict(color='white'),
+        fill=dict(color='white'),
+        align=['center'],
+        font=dict(color='black', size=12)
+    ),
+    cells=dict(
+        values=[df.Color, df.Year],
+        line=dict(color=[df.Color]),
+        fill=dict(color=[df.Color]),
+        align='center',
+        font=dict(color='black', size=11)
     ))
 
 data = [trace0]
 
-py.iplot(data, filename = "row variable color")
+py.iplot(data, filename="row variable color")
